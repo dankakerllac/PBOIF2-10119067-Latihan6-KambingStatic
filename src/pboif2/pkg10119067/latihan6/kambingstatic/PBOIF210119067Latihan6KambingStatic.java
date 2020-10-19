62d4f4e9414807ce180dc24a5c5c492964fc5561
dankakerllac <72844005+dankakerllac@users.noreply.github.com>
2020-10-19 22:43:41 +0700

    Update PBOIF210119067Latihan6KambingStatic.java

--- src/pboif2/pkg10119067/latihan6/kambingstatic/PBOIF210119067Latihan6KambingStatic.java
+++ src/pboif2/pkg10119067/latihan6/kambingstatic/PBOIF210119067Latihan6KambingStatic.java
@@ -3,9 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pboif2.pkg10119067.latihan2.deklarasivariabel;
+package pboif2.pkg10119067.latihan2;
 
 /**
+ *
+ *
+ * NAMA     : Andreas Danhago
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi Program : Deklarasi & Pengaksesan
+ */
+public class PBOIF210119067 {
  *
  *
  * NAMA     : Andreas Danhago
