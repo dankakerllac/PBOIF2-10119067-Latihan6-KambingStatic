3fae06a803ae7edb020c870cb4ece1d5b81a1959
User <User@DESKTOP-6N6EHH6>
2020-10-19 22:23:42 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan6/kambingstatic/PBOIF210119067Latihan6KambingStatic.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan2.deklarasivariabel;
+
+/**
+ *
+ *
+ * NAMA     : Andreas Danhago
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi Program : Deklarasi & Pengaksesan
+ */
+public class PBOIF210119067Latihan1Biodata {
+ //NAMA_KAMBING sebagai konstanta
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + "memiliki kambing sebanyak " + Mamalia .jumlahKambing);
+    }
+   
+}
